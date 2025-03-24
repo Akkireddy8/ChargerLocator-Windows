2ef0b404d77553af63e06b89485fad8d0770571e
iamkarthikkasula99 <iamkarthikkasula99@gmail.com>
2025-03-23 21:40:49 -0500

    Fixed bugs

--- src/routers/stationRoutes.js
+++ src/routers/stationRoutes.js
@@ -6,5 +6,7 @@ router.get('/stations', stationController.getStations);
 router.post('/stations', stationController.addStation);
 router.put('/stations/:id', stationController.updateStation);
 router.delete('/stations/:id', stationController.deleteStation);
+router.delete('/stations/:stationId/chargers/:chargerId', stationController.deleteCharger);
+
 
 module.exports = router;
